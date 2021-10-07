0247620d98c8f07eabd6fd42eb0f38070ae553f8
Dion Pettaway <dionp@fullsail.edu>
2021-10-07 17:02:48 -0400

    Added a simple proof of stake to blockchain

--- Blockchain/Block.js
+++ Blockchain/Block.js
@@ -1,32 +1,34 @@
 const SHA384 = require('crypto-js/sha384');
 
 class Block{
-  #timestamp
-  #data
-  #previousHash
   constructor(timestamp = Date.now(), data, previousHash='') {
     this.timestamp = timestamp;
     this.data = data;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
+    this.nonce = 0;
   }
 
   calculateHash = () => {
-    return SHA384(this.timestamp + this.previousHash + JSON.stringify(this.data)).toString();
+    return SHA384(this.timestamp + this.previousHash + JSON.stringify(this.data) + this.nonce).toString();
   }
 
-  getPrev () {
-    return this.#previousHash
+  mineBlock(difficulty) {
+    while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+    console.log("Block mined: ", this.hash);
   }
 }
+// const block = new Block(Date.now(), "0x38903fd342543", "0")
 
-const block = new Block(Date.now(), "0x38903fd342543", "0")
-
-console.log(block[this.data], block)
+// console.log({block})
 
 class Blockchain {
   constructor() {
-    this.chain = [this.createGenesisBlock()]
+    this.chain = [this.createGenesisBlock()];
+    this.difficulty = 4;
   }
 
   createGenesisBlock = () => {
@@ -38,18 +40,31 @@ class Blockchain {
   }
 
   addBlock (newBlock) {
-    newBlock.getprev = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash();
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.mineBlock(this.difficulty);
     this.chain.push(newBlock)
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      // Start at 1 as that'll set the previous block as the genesis block
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+      // Bc the data in the block shouldn't change, the hash should stay the same no matter how many times you rehash.
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false
+      }
+    }
+    return true;
   }
 }
 
 let riekiCoin = new Blockchain();
-
+console.log("Mining block 1....")
 riekiCoin.addBlock(new Block(Date.now(), {amount: 4399}));
+console.log("Mining block 2....")
 riekiCoin.addBlock(new Block(Date.now(), {amount: 1889891}));
-console.log(JSON.stringify(riekiCoin, null, 4));
-// riekiCoin.addBlock(new Block(new Date(), {amount: 3}));
-// riekiCoin.addBlock(new Block(new Date(), {amount: 2343}));
-
-// console.log(JSON.stringify(riekiCoin, null, 4));
