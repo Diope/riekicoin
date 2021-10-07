4c1f7c08dfde99f0ae085805d1654f8cc96a2789
Dion Pettaway <dionp@fullsail.edu>
2021-10-07 17:27:28 -0400

    Code clean up and separation of classes

--- Blockchain/Block.js
+++ Blockchain/Block.js
@@ -1,9 +1,9 @@
 const SHA384 = require('crypto-js/sha384');
 
 class Block{
-  constructor(timestamp = Date.now(), data, previousHash='') {
+  constructor(timestamp = Date.now(), transaction, previousHash='') {
     this.timestamp = timestamp;
-    this.data = data;
+    this.transaction = transaction;
     this.previousHash = previousHash;
     this.hash = this.calculateHash();
     this.nonce = 0;
@@ -21,50 +21,5 @@ class Block{
     console.log("Block mined: ", this.hash);
   }
 }
-// const block = new Block(Date.now(), "0x38903fd342543", "0")
 
-// console.log({block})
-
-class Blockchain {
-  constructor() {
-    this.chain = [this.createGenesisBlock()];
-    this.difficulty = 4;
-  }
-
-  createGenesisBlock = () => {
-    return new Block(Date.now(), "The beginning of all blocks", "0");
-  }
-
-  getLatestBlock = () => {
-    return this.chain[this.chain.length - 1];
-  }
-
-  addBlock (newBlock) {
-    newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.mineBlock(this.difficulty);
-    this.chain.push(newBlock)
-  }
-
-  isChainValid() {
-    for (let i = 1; i < this.chain.length; i++) {
-      // Start at 1 as that'll set the previous block as the genesis block
-      const currentBlock = this.chain[i];
-      const previousBlock = this.chain[i - 1];
-      // Bc the data in the block shouldn't change, the hash should stay the same no matter how many times you rehash.
-      if (currentBlock.hash !== currentBlock.calculateHash()) {
-        return false;
-      }
-
-      if (currentBlock.previousHash !== previousBlock.hash) {
-        return false
-      }
-    }
-    return true;
-  }
-}
-
-let riekiCoin = new Blockchain();
-console.log("Mining block 1....")
-riekiCoin.addBlock(new Block(Date.now(), {amount: 4399}));
-console.log("Mining block 2....")
-riekiCoin.addBlock(new Block(Date.now(), {amount: 1889891}));
+module.exports = {Block};
--- Blockchain/Blockchain.js
+++ Blockchain/Blockchain.js
@@ -1,4 +1,63 @@
+const {Block} = require('./Block')
+const {Transaction} = require('./Transaction')
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+    this.difficulty = 4;
+    this.pendingTransactions = [];
+    this.miningReward = 400
+  }
+
+  createGenesisBlock = () => {
+    return new Block(Date.now(), "The beginning of all blocks", "0");
+  }
+
+  getLatestBlock = () => {
+    return this.chain[this.chain.length - 1];
+  }
+
+  // addBlock (newBlock) {
+  //   newBlock.previousHash = this.getLatestBlock().hash;
+  //   newBlock.mineBlock(this.difficulty);
+  //   this.chain.push(newBlock)
+  // }
+
+  minePendingTransactions(miningRewardAddress) {
+    let block = new Block(Date.now(), this.pendingTransactions)
+    block.mineBlock(this.difficulty);
+
+    console.log('Mined block hash is: ', block.hash)
+    this.chain.push(block);
+
+    this.pendingTransactions = [new Transaction(null, this.miningReward, this.miningReward)];
+  }
+
+  createTransaction(transaction) {
+    this.pendingTransactions.push(transaction)
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      // Start at 1 as that'll set the previous block as the genesis block
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+      // Bc the data in the block shouldn't change, the hash should stay the same no matter how many times you rehash.
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false
+      }
+    }
+    return true;
+  }
+}
 
 
 
-
+let riekiCoin = new Blockchain();
+console.log("Mining block 1....")
+riekiCoin.minePendingTransactions(new Block(Date.now(), {amount: 4399}));
+console.log("Mining block 2....")
+riekiCoin.minePendingTransactions(new Block(Date.now(), {amount: 1889891}));
